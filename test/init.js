ceafa445b6c19614e88f46a491efa09146f0f3b1
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-27 18:35:24 +0400

    Move to mocha, fix undefined and escaping issue

---
+++ test/init.js
@@ -0,0 +1,9 @@
+var Schema = require('jugglingdb').Schema;
+
+global.getSchema = function() {
+    var db = new Schema(require('../'), {
+        database: ':memory:'
+    });
+    // db.log = function (a) { console.log(a); };
+    return db;
+};
--- test/sqlite3.js
+++
@@ -1,9 +0,0 @@
-var jdb = require('jugglingdb'),
-    Schema = jdb.Schema,
-    test = jdb.test,
-    schema = new Schema(__dirname + '/..', {
-        database: ':memory:'
-    });
-
-test(module.exports, schema);
-
