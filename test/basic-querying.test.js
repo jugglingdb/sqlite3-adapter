0067b23a029eb8750fbb9a1838a256cff23d4c47
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-16 23:59:06 +0100

    Test conditions

--- test/basic-querying.test.js
+++ test/basic-querying.test.js
@@ -1,117 +1,193 @@
-var should = require('./init.js');
-var Schema = require('jugglingdb').Schema;
-var db, UserData;
+const should = require('./init.js');
+let db, UserData;
 
-describe('basic-query-sqlite', function () {
-  before(setup);
-  before(function (done) {
-    db = getSchema();
-    UserData = db.define('UserData', {
-      name : String,
-      email : String,
-      role : String,
-      order : Number
+/* global getSchema */
+
+describe('basic-query-sqlite', function() {
+    before(setup);
+
+    before(done => {
+        db = getSchema();
+        UserData = db.define('UserData', {
+            name : String,
+            email : String,
+            role : String,
+            order : Number
+        });
+
+        db.automigrate(done);
     });
 
-    db.automigrate(done);
-  });
-  
-  before(seed);
-  
-  it('should query collection using or operator', function (done) {
-    UserData.all({
-      where : {
-        or : [{
-          name : 'Paul McCartney'
-        }, {
-          name : 'John Lennon'
-        }]
-      }
-    }, function (err, users) {
-      should.exists(users);
-      should.not.exists(err);
-      users.should.have.lengthOf(2);
-      users.forEach(function (u) {
-        u.role.should.eql('lead');
-      });
-      done();
-    });
-  });
-  
-   it('should query collection using or operator on different fields', function (done) {
-    UserData.all({
-      where : {
-        or : [{
-          name : 'Not a User'
-        }, {
-          order : '5'
-        }]
-      }
-    }, function (err, users) {
-      should.exists(users);
-      should.not.exists(err);
-      users.should.have.lengthOf(1);
-      users[0].order.should.eql(5);
-      done();
-    });
-  });
+    before(seed);
 
-  it('should query collection using or operator combined with and operator', function (done) {
-    UserData.all({
-      where : {
-        name : 'Ringo Starr',
-        or : [{
-          role : 'lead'
-        }, {
-          order : '6'
-        }]
-      }
-    }, function (err, users) {
-      should.exists(users);
-      should.not.exists(err);
-      users.should.have.lengthOf(1);
-      users[0].name.should.equal('Ringo Starr');
-      done();
+    it('should query collection using or operator', () => {
+        return UserData.all({
+            where : {
+                or : [{
+                    name : 'Paul McCartney'
+                }, {
+                    name : 'John Lennon'
+                }]
+            }
+        })
+            .then(users => {
+                should.exist(users);
+                users.should.have.lengthOf(2);
+                users.forEach(function(u) {
+                    u.role.should.eql('lead');
+                });
+            });
     });
-  });
+  
+    it('should query collection using or operator on different fields', function() {
+        return UserData.all({
+            where : {
+                or : [{
+                    name : 'Not a User'
+                }, {
+                    order : '5'
+                }]
+            }
+        })
+            .then(users => {
+                should.exists(users);
+                users.should.have.lengthOf(1);
+                users[0].order.should.eql(5);
+            });
+    });
+
+    it('should query collection using or operator combined with and operator', () => {
+        return UserData.all({
+            where : {
+                name : 'Ringo Starr',
+                or : [{
+                    role : 'lead'
+                }, {
+                    order : '6'
+                }]
+            }
+        })
+            .then(users => {
+                should.exists(users);
+                users.should.have.lengthOf(1);
+                users[0].name.should.equal('Ringo Starr');
+            });
+    });
+
+    it('should query by null', () => {
+        return UserData.findOne({ where: { email: null }})
+            .then(user => {
+                should.not.exist(user.email);
+            });
+    });
+
+    it('should support exclusion from empty set', () => {
+        return UserData.count({ email: { nin: [] }})
+            .then(count => {
+                count.should.equal(6); // full set
+            });
+    });
+
+    it('should support exclusion from non empty set', () => {
+        return UserData.count({ order: { nin: [ 1 ] }})
+            .then(count => {
+                count.should.equal(5);
+            });
+    });
+
+    it('should support inclusion in empty set', () => {
+        return UserData.count({ email: { inq: [] }})
+            .then(count => {
+                count.should.equal(0); // empty set
+            });
+    });
+
+    it('should query by "gt"', () => {
+        return UserData.count({ order: { gt: 2 }})
+            .then(count => {
+                count.should.equal(4);
+            });
+    });
+
+    it('should query by "gte"', () => {
+        return UserData.count({ order: { gte: 2 }})
+            .then(count => {
+                count.should.equal(5);
+            });
+    });
+
+    it('should query by "lt"', () => {
+        return UserData.count({ order: { lt: 2 }})
+            .then(count => {
+                count.should.equal(1);
+            });
+    });
+
+    it('should query by "lte"', () => {
+        return UserData.count({ order: { lte: 2 }})
+            .then(count => {
+                count.should.equal(2);
+            });
+    });
+
+    it('should query by "ne"', () => {
+        return UserData.count({ order: { ne: 2 }})
+            .then(count => {
+                count.should.equal(5);
+            });
+    });
+
+    it('should query by using "LIKE"', () => {
+        return UserData.count({ email: { like: '%b3atl3s%' }})
+            .then(count => {
+                count.should.equal(2);
+            });
+    });
+
+    it('should query by using "NLIKE"', () => {
+        return UserData.count({ email: { nlike: '%paul%' }})
+            .then(count => {
+                count.should.equal(1);
+            });
+    });
+
+    it('should query by using "BETWEEN"', () => {
+        return UserData.count({ order: { between: [ 3, 5 ] }})
+            .then(count => {
+                count.should.equal(3);
+            });
+    });
+
 }); 
- 
-function seed(done) {
-  var count = 0;
-  var beatles = [{
-    name : 'John Lennon',
-    mail : 'john@b3atl3s.co.uk',
-    role : 'lead',
-    order : 2
-  }, {
-    name : 'Paul McCartney',
-    mail : 'paul@b3atl3s.co.uk',
-    role : 'lead',
-    order : 1
-  }, {
-    name : 'George Harrison', order : 5
-  }, {
-    name : 'Ringo Starr', order : 6
-  }, {
-    name : 'Pete Best', order : 4
-  }, {
-    name : 'Stuart Sutcliffe', order : 3
-  }];
-  UserData.destroyAll(function () {
-    beatles.forEach(function (beatle) {
-      UserData.create(beatle, ok);
-    });
-  });
 
-  function ok(err) {
-    if (++count === beatles.length) {
-      done();
-    }
-  }
+function seed() {
+    const beatles = [{
+        name : 'John Lennon',
+        email : 'john@b3atl3s.co.uk',
+        role : 'lead',
+        order : 2
+    }, {
+        name : 'Paul McCartney',
+        email : 'paul@b3atl3s.co.uk',
+        role : 'lead',
+        order : 1
+    }, {
+        name : 'George Harrison', order : 5
+    }, {
+        name : 'Ringo Starr', order : 6
+    }, {
+        name : 'Pete Best', order : 4
+    }, {
+        name : 'Stuart Sutcliffe', order : 3
+    }];
+
+    return UserData.destroyAll()
+        .then(() => Promise.all(beatles.map(beatle => UserData.create(beatle))));
 }
 
 function setup(done) {
-  require('./init.js');
-  db = getSchema();
-  done();
+    require('./init.js');
+    db = getSchema();
+    done();
 }
+
